300130733e11a27ba700e3b12af6d43c125d9f8f
adam <amartenj@gmail.com>
2017-06-20 13:57:58 -0700

    Complete phase 1

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -6,7 +6,7 @@ class CatsController < ApplicationController
   end
 
   def show
-    @cat = Cat.find_by(id: params[:id])
+    @cat = current_cat
     if @cat
       render :show
     else
@@ -19,7 +19,37 @@ class CatsController < ApplicationController
     render :new
   end
 
+  def edit
+    @cat = current_cat
+    render :edit
+  end
+
+  def update
+    @cat = current_cat
+    if @cat.update_attributes(cat_pawrams)
+      redirect_to cat_url(@cat)
+    else
+      render json: 'cannae update cat', status: 404
+    end
+  end
+
+  def create
+    @cat = Cat.new(cat_pawrams)
+    if @cat.save
+      redirect_to cats_url
+    else
+      render json: 'cannae find cat', status: 404
+    end
+  end
+
+
   private
-  def cat_params
+
+  def current_cat
+    Cat.find_by(id: params[:id])
+  end
+
+  def cat_pawrams
+    params.require(:cat).permit(:name, :sex, :birth_date, :description, :color)
   end
 end
